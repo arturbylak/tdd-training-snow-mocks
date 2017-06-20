4bdf10f02e65ab075e85431ef80aa6c4095a194b
abylak <ABylak@pl.imshealth.com>
2017-06-20 12:39:47 +0200

    Test

--- src/main/java/snow/SnowRescueService.java
+++ src/main/java/snow/SnowRescueService.java
@@ -5,11 +5,21 @@ import snow.dependencies.PressService;
 import snow.dependencies.WeatherForecastService;
 
 public class SnowRescueService {
+    private final WeatherForecastService weatherForecastService;
+    private final MunicipalServices municipalServices;
+    private final PressService pressService;
 
-	public SnowRescueService(WeatherForecastService weatherForecastService, MunicipalServices municipalServices, PressService pressService) {
-	}
+    public SnowRescueService(WeatherForecastService weatherForecastService, MunicipalServices municipalServices, PressService pressService) {
+        this.weatherForecastService = weatherForecastService;
+        this.municipalServices = municipalServices;
+        this.pressService = pressService;
+    }
 
-	public void checkForecastAndRescue() {
-	}
+    public void checkForecastAndRescue() {
+        int averageTemperatureInCelsius = weatherForecastService.getAverageTemperatureInCelsius();
 
+        if (averageTemperatureInCelsius < 0) {
+            municipalServices.sendSander();
+        }
+    }
 }
--- src/test/java/snow/SnowRescueServiceTest.java
+++ src/test/java/snow/SnowRescueServiceTest.java
@@ -1,4 +1,70 @@
 package snow;
 
+import org.junit.Before;
+import org.junit.Test;
+import snow.dependencies.MunicipalServices;
+import snow.dependencies.PressService;
+import snow.dependencies.WeatherForecastService;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 public class SnowRescueServiceTest {
+    private SnowRescueService snowRescueService;
+    private WeatherForecastService weatherForecastService;
+    private MunicipalServices municipalServices;
+    private PressService pressService;
+
+    @Before
+    public void setUp() {
+        weatherForecastService = mock(WeatherForecastService.class);
+        pressService = mock(PressService.class);
+        municipalServices = mock(MunicipalServices.class);
+
+        snowRescueService = new SnowRescueService(weatherForecastService, municipalServices, pressService);
+    }
+
+    @Test
+    public void shouldSendSanderBecauseOfTemperatureLessThan0() {
+        //given
+        int temperatureInCelsius = -5;
+
+        when(weatherForecastService.getAverageTemperatureInCelsius()).thenReturn(temperatureInCelsius);
+
+        //when
+        snowRescueService.checkForecastAndRescue();
+
+        //then
+        verify(municipalServices).sendSander();
+    }
+
+    @Test
+    public void shouldNotSendSanderBecauseOfTemperatureMoreThan0Celsius() {
+        //given
+        int temperatureInCelsius = 3;
+
+        when(weatherForecastService.getAverageTemperatureInCelsius()).thenReturn(temperatureInCelsius);
+
+        //when
+        snowRescueService.checkForecastAndRescue();
+
+        //then
+        verify(municipalServices, never()).sendSander();
+    }
+
+    @Test
+    public void shouldNotSendSanderBecauseOfTemperatureEquals0Celsius() {
+        //given
+        int temperatureInCelsius = 0;
+
+        when(weatherForecastService.getAverageTemperatureInCelsius()).thenReturn(temperatureInCelsius);
+
+        //when
+        snowRescueService.checkForecastAndRescue();
+
+        //then
+        verify(municipalServices, never()).sendSander();
+    }
 }
